499305b808bf199125703dbeafdc97ee2497a0a6
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-26 15:07:07 +0300

    feat: setup routing
    * using createBrowserRouter instead of component Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import Cart from "@/features/cart/Cart";
+import Menu from "@/features/menu/Menu";
+import CreateOrder from "@/features/order/CreateOrder";
+import Order from "@/features/order/Order";
+import AppLayout from "@/ui/AppLayout";
+import Home from "@/ui/Home";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/menu", element: <Menu /> },
+      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/:orderId", element: <Order /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
-
