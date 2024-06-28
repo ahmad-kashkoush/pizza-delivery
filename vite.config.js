5a2152b1013259894c6e6c43ceb411b0dbc6d7fe
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-28 15:03:07 +0300

    chore: update import aliases
    - remove unnecessary @utils alias

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@utils":path.resolve(_direname, "./src/utils")
+
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+      // "@utils": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src/utils")
     }
   }
 })
