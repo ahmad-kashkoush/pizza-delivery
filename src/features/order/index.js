c149e6765fcc80de234a5d9c13a046ed0f271e01
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-28 15:07:56 +0300

    chore: update Imports & fix open cart Link

---
+++ src/features/order/index.js
@@ -0,0 +1,2 @@
+export {default as  Order } from "@/features/order/Order";
+export {default as CreateOrder } from "@/features/order/CreateOrder";
---
+++ src/ui/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "@/ui/Home";
+export { default as Header } from "@/ui/Header";
+export { default as Error } from "@/ui/Error";
+export { default as AppLayout } from "@/ui/AppLayout";
