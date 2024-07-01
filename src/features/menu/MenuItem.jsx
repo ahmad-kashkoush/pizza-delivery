0518b6f027075ef07125d39a4cbbc4219f9b68ae
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-01 12:46:30 +0300

    feat(MenuItem): add conditional style to image when sold out

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -6,7 +6,11 @@ function MenuItem({ pizza }) {
 
   return (
     <li className="mb-4 flex items-end gap-4 border-b-2 pb-2">
-      <img className="h-24 w-24" src={imageUrl} alt={name} />
+      <img
+        className={`h-24 w-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+        src={imageUrl}
+        alt={name}
+      />
       <div className="flex-1">
         <p className="text-semibold font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">
