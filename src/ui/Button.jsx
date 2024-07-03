2843862ecd9ee1920c8c8a5d4327cfe648710a2a
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-03 23:09:21 +0300

    feat(Button): add caller functions onClick

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,12 +6,14 @@ function Button({
   to,
   className = '',
   type = 'primary',
+  onClick,
 }) {
   const base = `rounded-full bg-yellow-400  font-semibold uppercase text-stone-800 transition-colors duration-300 hover:bg-yellow-500 disabled:cursor-not-allowed  ${className}`;
 
   const styles = {
     primary: base + ' text-sm  px-4 py-3 md:px-6 md:py-4',
     small: base + ' text-xs py-2 px-4 md:px-5 md:py-3',
+    smaller: base + ' text-xs py-2 px-3 ',
     secondary:
       'rounded-full border-2 px-3 py-1 text-sm font-semibold uppercase text-stone-500 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 md:px-5 md:py-3',
   };
@@ -21,6 +23,12 @@ function Button({
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
+    );
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
     );
   return (
     <button disabled={disabled} className={styles[type]}>
