d49cbbc7ec8e4ddcc876dd1ba77a55b8a9ceac9f
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-07-03 23:10:10 +0300

    feat(CartOverview): disaply data dynamically

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,19 @@
+import { getTotalPrice, getTotalQuantity } from '@/features/cart/cartSlice';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 function CartOverview() {
+  const totalQuantity = useSelector(getTotalQuantity);
+  const totalPrice = useSelector(getTotalPrice);
+  if (!totalQuantity) return null;
   return (
     <div className="flex justify-between bg-stone-800 px-4 py-3 uppercase text-stone-200">
-      <p className="font-semibold uppercase text-stone-300 flex gap-4">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+      <p className="flex gap-4 font-semibold uppercase text-stone-300">
+        <span>{totalQuantity} pizzas</span>
+        <span>${totalPrice}</span>
       </p>
-      <Link  to="/cart">Open cart &rarr;</Link>
+
+      <Link to="/cart">Open cart &rarr;</Link>
     </div>
   );
 }
